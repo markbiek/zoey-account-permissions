36fc0b970eb7d2e10a8083a6879cd3d40f4fd2e1
Mark Biek <support@janustech.net>
2021-12-22 12:17:37 -0500

    Model relationships

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -4,4 +4,12 @@ namespace Zoey\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model {
+	public function permissions() {
+		return $this->belongsToMany(
+			'Zoey\Models\Permissions',
+			'role_permissions',
+			'role_id',
+			'id',
+		);
+	}
 }
